a9e5b88b241ebc6501e345a4b70fe2909a4198c1
Amir2292 <amiamirsalehi@gmail.com>
2026-05-07 10:36:29 +0330

    add custom hook for managing post functionalities

---
+++ src/hooks/usePosts.ts
@@ -0,0 +1,55 @@
+import {useState} from "react";
+
+import {v4 as uuid} from "uuid";
+import {Post} from "../types/post";
+import {mockPosts} from "../data/mockPosts";
+
+export function usePosts() {
+  const [posts, setPosts] = useState<Post[]>(mockPosts);
+
+  const addPost = (content: string) => {
+    const newPost: Post = {
+      id: uuid(),
+      username: "you",
+      avatar: "https://i.pravatar.cc/40",
+      content,
+      likes: 0,
+      liked: false,
+      comments: [],
+    };
+
+    setPosts((prev) => [newPost, ...prev]);
+  };
+
+  const toggleLike = (id: string) => {
+    setPosts((prev) =>
+      prev.map((p) =>
+        p.id === id
+          ? {
+              ...p,
+              liked: !p.liked,
+              likes: p.liked ? p.likes - 1 : p.likes + 1,
+            }
+          : p,
+      ),
+    );
+  };
+
+  const addComment = (postId: string, text: string) => {
+    setPosts((prev) =>
+      prev.map((p) =>
+        p.id === postId
+          ? {
+              ...p,
+              comments: [
+                ...p.comments,
+                {id: uuid(), username: "you", content: text},
+              ],
+            }
+          : p,
+      ),
+    );
+  };
+
+  return {posts, addPost, toggleLike, addComment};
+}
